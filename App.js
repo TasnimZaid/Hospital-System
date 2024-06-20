fa62305567ff248294f9c5cf8ac06c342a647aa2
TasnimZaid <tasnim.aboarqob@gmail.com>
2024-06-20 20:25:17 +0300

    regex

--- App.js
+++ App.js
@@ -1,27 +1,83 @@
-function render(event){
+function validForm() {
+  const fullName = document.getElementById('fullName').value;
+  const password = document.getElementById('pwd').value;
+  const tel = document.getElementById('tel').value ;
+  const email = document.getElementById('email').value ;
 
-    event.preventDefault();
-    let userInf = document.getElementById("user-inf") ; 
-    let form = event.target ; 
-    let fullName = form.fullName.value ;
-    let password = form.pwd.value ;
-    let dateofbirth = form.date.value ;
-    let gender = form.gender.value ;
-    let tel = form.tel.value ;
-    let chronicdisease = form.chronicdisease.value ;
-    let img = form.img.value;
+  // Regular expressions for each criteria
+  const nameRegex = /^[^\s]*$/;
+  const lengthRegex = /.{8,}/;
+  const lowercaseRegex = /[a-z]/;
+  const uppercaseRegex = /[A-Z]/;
+  const digitRegex = /\d/;
+  const specialRegex = /[!@#$%^&*()|<>]/;
+  const telregex = /^07\d{8}$/;
+  const emailregex = /^[^\s@]+@[^\s@]+\.com$/;
 
+  // valid for email : 
+  const isemailvalid = validCondition(emailregex , "emailvalid" , email);
 
-    const imageforpatients = document.getElementById("imageforpatients");
-    const newimg = document.createElement("img");
-    newimg.src = img;
-    imageforpatients.appendChild(newimg);
+  // vlidation for phone num 
+  const istellvalid = validCondition(telregex , "telvalid" , tel)
 
-    
+  // Validation for full name
+  const isNameValid = validCondition(nameRegex, "withoutspace", fullName);
 
+  // Validation for password criteria
+  const isLengthValid = validCondition(lengthRegex, "length", password);
+  const isLowercaseValid = validCondition(lowercaseRegex, "lowercase", password);
+  const isUppercaseValid = validCondition(uppercaseRegex, "uppercase", password);
+  const isDigitValid = validCondition(digitRegex, "digit", password);
+  const isSpecialValid = validCondition(specialRegex, "special", password);
 
-    
-    let formData = {
+  // Return true if all conditions are valid
+  return isemailvalid&&istellvalid && isNameValid && isLengthValid && isLowercaseValid && isUppercaseValid && isDigitValid && isSpecialValid;
+}
+
+function validCondition(regex, elementId, value) {
+  const element = document.getElementById(elementId);
+  const isValid = regex.test(value);
+
+  if (isValid) {
+      element.classList.remove("invalid");
+      element.classList.add("valid");
+  } else {
+      element.classList.remove("valid");
+      element.classList.add("invalid");
+  }
+
+  return isValid;
+}
+
+function render(event) {
+  event.preventDefault();
+  const isFormValid = validForm();
+
+  if (isFormValid) {
+      // Proceed with form submission or other actions
+      console.log("Form is valid. Submitting...");
+  } else {
+      console.log("Form is invalid. Please correct errors.");
+      return; // Prevent further execution if form is invalid
+  }
+
+  let form = event.target;
+  let fullName = form.fullName.value;
+  let password = form.pwd.value;
+  let dateofbirth = form.date.value;
+  let gender = form.gender.value;
+  let tel = form.tel.value;
+  let chronicdisease = form.chronicdisease.value;
+  let img = form.img.value;
+
+  // Append new img to insert it by user
+  const imageforpatients = document.getElementById("imageforpatients");
+  const newimg = document.createElement("img");
+  newimg.src = img;
+  imageforpatients.appendChild(newimg);
+
+  // Create an object to store form data
+  let formData = {
       fullName: fullName,
       password: password,
       dateofbirth: dateofbirth,
@@ -31,91 +87,63 @@ function render(event){
       img: img
   };
 
-  // localstorage get and set data :
-      
-    //  let inf = localStorage.getItem('inf');
-    //   inf = JSON.parse(inf);
-    //   if (inf) {
-    //     console.log(inf);
-    // } else {
-    //     console.log('No value found for the key "inf" in localStorage.');
-    // }
-    //  inf.push(inf);
-    //  localStorage.setItem('inf', JSON.stringify(inf));
-
-      let inf = localStorage.getItem('inf');        
-      if (inf) {
+  // Get existing data from local storage
+  let inf = localStorage.getItem('formData');
+  if (inf) {
       inf = JSON.parse(inf);
-     } else {
+  } else {
       inf = [];
-      }
+  }
 
-      inf.push(formData);
-      localStorage.setItem('inf', JSON.stringify(inf));
+  // Add new data to array and save back to local storage
+  inf.push(formData);
+  localStorage.setItem('formData', JSON.stringify(inf));
 
-      userInf.innerHTML = '';
-      let cardDiv = document.getElementById('card') ;
-
-      inf.forEach(data => {
-        let newDiv = document.createElement('div') ;
-        cardDiv.appendChild(newDiv) ;
-        newDiv.style.width = 'fit-content';
-        newDiv.style.display = 'flex' ;
-        newDiv.style.flexDirection = "row";        
-        newDiv.style.background = 'white' ;
-        newDiv.style.height = "fit-content" ;
-        newDiv.style.padding = "20px";
-        newDiv.style.margin = "20px";
-
-        
-
-
-
-          let userInfoHTML = "Full Name: " + data.fullName + "<br>" 
-                           + "Password: " + data.password + "<br>" 
-                           + "Date of Birth: " + data.dateofbirth + "<br>" 
-                           + "Gender: " + data.gender + "<br>" 
-                           + "Tel Number: " + data.tel + "<br>" 
-                           + "Chronic Disease: " + data.chronicdisease + "<br><br>";
-  
-          newDiv.innerHTML = userInfoHTML;
-
-  
-          userInf.appendChild(newDiv);
-
-  
-          const imgElement = document.createElement("img");
-          imgElement.src = data.img;
-          userInf.appendChild(imgElement);
-
-      });
-  
-
-
-
+  // Clear previous display and show updated data
+  displayData(inf);
 }
 
-  // Clear local storage
+function displayData(inf) {
+  let userInf = document.getElementById("user-inf");
+  userInf.innerHTML = '';
 
-let clear = document.getElementById('clear') ;
+  inf.forEach(data => {
+      let newDiv = document.createElement('div');
+      newDiv.classList.add('user-card');
+
+      let userInfoHTML = `
+          Full Name: ${data.fullName}<br>
+          Password: ${data.password}<br>
+          Date of Birth: ${data.dateofbirth}<br>
+          Gender: ${data.gender}<br>
+          Tel Number: ${data.tel}<br>
+          Chronic Disease: ${data.chronicdisease}<br><br>
+      `;
+
+      newDiv.innerHTML = userInfoHTML;
+
+      const imgElement = document.createElement("img");
+      imgElement.src = data.img;
+      newDiv.appendChild(imgElement);
+
+      userInf.appendChild(newDiv);
+  });
+}
 
 function clearData() {
   localStorage.clear();
-  inf = [];
-
-  
+  document.getElementById("user-inf").innerHTML = '';
+  document.getElementById("imageforpatients").innerHTML = '';
 }
 
-clear.addEventListener("click" , clearData );
+document.getElementById('clear').addEventListener("click", clearData);
+document.getElementById('form').addEventListener("submit", render);
 
-  
-
-let form = document.getElementById('form') ;
-form.addEventListener("submit" , render );
-
-
-
-
-
-
-
+// Ensure form data is displayed on page load
+window.onload = function() {
+  let inf = localStorage.getItem('formData');
+  if (inf) {
+      inf = JSON.parse(inf);
+      displayData(inf);
+  }
+}
